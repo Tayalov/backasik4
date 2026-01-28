9fbc21ab2ef9d0fa23dc3e875256e9642e3ae79d
Tayalov <241752@astanait.edu.kz>
2026-01-28 14:15:10 +0500

    Create authMiddleware.js

---
+++ authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const protect = async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (err) {
+      res.status(401);
+      return next(new Error('Not authorized, token failed'));
+    }
+  } else {
+    res.status(401);
+    return next(new Error('Not authorized, no token'));
+  }
+};
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403);
+    return next(new Error('Admin only access'));
+  }
+};
+
+module.exports = { protect, admin };
